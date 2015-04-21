102f3ecd389d69b2d230bf6e051c54dae5b1bc3a
kaitlinmuth <kaitlin.muth@gmail.com>
2015-04-21 16:30:41 -0500

    Slide up/down functionality! Also, pretty.

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ function randomNumber(min, max){
 
 $(document).ready(function() {
 	$("#btn").on('click', function(){
+
 		var newProperty = new Property();
-		//var newRow = "<ul><li>Property ID: " + newProperty.propId + "</li><li>" + newProperty.sqFt
-		var newRow = "<div id=row><div id=cell>" + "Property ID: " + newProperty.propId +"</div><div id=cell>" + newProperty.sqFt + " SQFT </div><div id=cell>" + "$" + newProperty.price + " P/SqFt</div><div id=cell><button class='removeButton'>Remove</button></div></div>";
+		var newRow = "<div id=row>" +
+						"<div id=cell> Property ID: " + newProperty.propId + "</div>" +
+						"<div id=cell>" + newProperty.sqFt + " SQFT </div>" +
+						"<div id=cell>$" + newProperty.price + "P/SqFt</div>" +
+						"<div id=cell><button class='removeButton'>Remove</button></div>" +
+						"</div>";
+
 		$("#propTable").prepend(newRow);
+		$("#propTable").children().first().hide().slideToggle();
 	});
 
 	$("#propTable").on('click', '.removeButton', function(){
-		$(this).parent().parent().slideUp();
+		$(this).parent().parent().slideToggle();
 		});
 	});
